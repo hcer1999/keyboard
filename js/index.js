9a43b19aad6e5862c4b8f0e6f967a99f4a07086b
Hcer <bingkelele007@163.com>
2020-02-04 17:26:18 +0800

    添加指示灯和logo

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
-window.onload = () => {
-  console.log("Var 1.0测试版");
-
+window.onload = event => {
+  console.log("Ver 1.1");
   isPhone(navigator.userAgent);
 };
 
@@ -22,7 +21,39 @@ let keys = document.querySelectorAll(".key");
 //绑定点击事件
 keys.forEach((item, index, arr) => {
   item.addEventListener("click", () => {
-    showText(item.getAttribute("id"));
+    let id = item.getAttribute("id");
+    let pilot = document.querySelectorAll(".pilot i");
+    showText(id);
+    // 检测点击切换大小写按钮控制指示灯的开关
+    if (id === "CapsLock") {
+      if (item.className.indexOf("active") === -1) {
+        item.classList.add("active");
+        pilot[1].classList.add("open");
+      } else {
+        item.classList.remove("active");
+        pilot[1].classList.remove("open");
+      }
+    }
+    //检测点击数字锁定按钮控制指示灯
+    if (id === "NumLock") {
+      if (item.className.indexOf("active") === -1) {
+        item.classList.add("active");
+        pilot[0].classList.add("open");
+      } else {
+        item.classList.remove("active");
+        pilot[0].classList.remove("open");
+      }
+    }
+    ////检测点击滚动锁定按钮控制指示灯
+    if (id === "ScrollLock") {
+      if (item.className.indexOf("active") === -1) {
+        item.classList.add("active");
+        pilot[2].classList.add("open");
+      } else {
+        item.classList.remove("active");
+        pilot[2].classList.remove("open");
+      }
+    }
   });
 });
 
@@ -38,7 +69,7 @@ document.body.addEventListener("keydown", e => {
   } else {
     keyEl.classList.remove("active");
   }
-  // console.log(e);
+  console.log(e);
 });
 
 function showText(keyText) {
